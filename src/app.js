2ad143edfded3c1e5edf9edaed480f751c00b41c
Akintoye Peterpaul <appeimisic@gmail.com>
2025-11-21 21:29:51 +0100

    trying to deploy

--- src/app.js
+++ src/app.js
@@ -3,8 +3,26 @@ const app = express();
 const connectDB = require("./config/db.js");
 const appRouter = require("./route.js");
 const StatusCodes = require("./utils/StatusCodes.js");
+const cors = require("cors");
 
 connectDB();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://my-next-ebon-eta.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("CORS policy violation: Origin not allowed"));
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +31,7 @@ app.get("/", (_req, res) => {
   res.send("Welcome to the Spent API");
 });
 
-app.use("/api", appRouter)
+app.use("/api", appRouter);
 
 app.use((_req, res) => {
   res.status(StatusCodes.NOT_FOUND).send({
